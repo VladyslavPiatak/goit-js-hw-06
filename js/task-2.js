bedf39506d2f1ec0e64e9f2e5d0f01993fff9b4b
VladyslavPiatak <vladislawatson@gmail.com>
2024-02-20 19:20:46 +0200

    work

---
+++ js/task-2.js
@@ -0,0 +1,28 @@
+class Storage {
+    #items = [];
+    constructor(items) {
+        this.#items = items;
+    }
+    getItems() {
+        return this.#items;
+    }
+    addItem(newItem) {
+        this.#items.push(newItem)
+    }
+    removeItem(itemToRemove) {
+        const deleteIndex = this.#items.indexOf(itemToRemove);
+        if (deleteIndex !== -1) {
+            this.#items.splice(deleteIndex, 1);
+        } else {
+            console.log("Такого товару не існує!");
+        }
+    }
+}
+
+
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
